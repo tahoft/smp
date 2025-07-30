871fb0eb60e435561f9f28787076f58821b7e61b
Thomas Hoft <hoft@stthomas.edu>
2025-07-30 14:39:56 -0500

    cleanup unused code, figure numbers, PLOTS flag, tweak smoothing param

--- smp_angle.m
+++ smp_angle.m
@@ -1,12 +1,21 @@
+%% smp_angle
+% Extract angle of 'U'-shaped shape memory polymers
+% For Brittany Nelson-Cheeseman, Elsie Kmecak, Abeer Najajra
+%
+% by Thomas Höft 
+
 %% load
 %#ok<*UNRCH>
 
-fn = 'IMG_8778.jpeg';
+fn = 'IMG_8776.jpeg';
+[fn_pth,fn_name,~] = fileparts(fn);
+
 im_orig = imread(fn);
+im_orig = rot90(im_orig,-1);
 figure(1); imagesc(im_orig); axis image; colormap gray; colorbar;
 
 PINK = true; 
-SAVE = true;
+SAVE = false;
 PLOTS = true;
 MOUNT_DELETE = false; % selectable region to get rid of black mount
 LINE_COLOR = 'w'; % color of line on final image: 
@@ -56,8 +65,8 @@ im_trim = im_gray(trim_info(2):trim_info(2)+trim_info(4), ...
 im_orig_trim = im_orig(trim_info(2):trim_info(2)+trim_info(4), ...
     trim_info(1):trim_info(1)+trim_info(3),:);
 [M,N]=size(im_trim);
-figure(2002); imagesc(im_trim); axis image; colormap gray; colorbar;
-title('trimmed image');
+figure(2); imagesc(im_trim); axis image; colormap gray; colorbar;
+title('trimmed grayscale image');
 
 if MOUNT_DELETE
     % also have user select region to get rid of any mounts
@@ -109,12 +118,15 @@ se1 = strel("disk",round(ker_d/4));
 % reflections: 
 im_bw = imclose(im_bw, se1); % dilation followed by erosion
 
-figure(2); imagesc(im_bw); axis image; colormap gray; colorbar;
+if PLOTS
+    figure(100); imagesc(im_bw); axis image; colormap gray; colorbar;
+end
 
 % get skeleton (bwskel)
 im_skel = bwskel(logical(im_bw));% bwskel needs logical not double
-figure(21); imagesc(im_bw+im_skel); axis image; colorbar;
-
+if PLOTS
+    figure(101); imagesc(im_bw+im_skel); axis image; colorbar;
+end
 
 %%
 % get list of (x,y) co-ordinates of each pixel in in (regionprops PixelList)
@@ -182,32 +194,18 @@ end
 %crv_xy = crv_xy(round(n/16):end-round(n/16),:);
 
 % double-check that curve is correct;
-tmp_im = zeros(M,N); tmp_im(sub2ind([M,N],crv_xy(:,2),crv_xy(:,1))) = 1;
-figure(701); imagesc(tmp_im); axis image; colormap gray; colorbar;
-
+if PLOTS
+    tmp_im = zeros(M,N); tmp_im(sub2ind([M,N],crv_xy(:,2),crv_xy(:,1))) = 1;
+    figure(110); imagesc(tmp_im); axis image; colormap gray; colorbar;
+end
 
 %% separate into flat & curved: 
 
 %  compute curvature at each point
 x = crv_xy(:,1); y = crv_xy(:,2); 
 crv_len = length(x);
-% %wid = 80;
-% %wid = 100;
-% wid = crv_len/8;
-% %x = smoothdata(x,"movmean",wid); y = smoothdata(y,"movmean",wid);
-% %x = smooth(x, 21);
-% dx = gradient(x); % central diff in middle, forward diff at ends
-% dx = smoothdata(dx,"movmean",wid); 
-% %dx = smooth(dx,wid); 
-% dy = gradient(y);
-% dy = smoothdata(dy,"movmean",wid);
-% ddx = gradient(dx); % 2nd order central diff
-% ddx = smoothdata(ddx,"movmean",wid); 
-% %ddx = smooth(ddx,wid);
-% ddy = gradient(dy); % 2nd order central diff
-% ddy = smoothdata(ddy,"movmean",wid);
 
-wid = round(crv_len/8);
+wid = round(crv_len/4); 
 %wid = 51;
 x = smooth(x,wid,'loess');
 dx = gradient(x);
@@ -223,24 +221,7 @@ ddy = smooth(ddy,wid,'loess');
 
 k = (dx.*ddy-dy.*ddx)./(dx.^2+dy.^2).^1.5;
 k = abs(k);
-%k = smoothdata(k,"movmedian",11);
-%k = smoothdata(k,"movmean",wid);
 k = smooth(k,wid);
-
-
-% trim off first & last wid to avoid any weirdness there from smoothing
-% window
-%n = size(crv_xy,1);
-%k = k(wid+1:end-wid,:);
-
-
-
-%ddy_nonz = ddx~=0;
-%CURVATURE_THRESH = max(k)/2;
-%CURVATURE_THRESH = max(k(wid+1:end-wid-1))/4;
-%CURVATURE_THRESH = (max(k(wid+1:end-wid-1))-min(k(wid+1:end-wid-1)))/4 + min(k(wid+1:end-wid-1));
-%kap_zer(wid+1:end-wid) = k < CURVATURE_THRESH;
-%kap_zer = k < CURVATURE_THRESH;
 
 % expect a W shape, so find two minima
 % the max inbetween helps find the thresholds we want
@@ -259,30 +240,26 @@ kap_zer(idmaxmid:end) = k(idmaxmid:end)<kap_thresh2;
 
 
 if PLOTS
-    figure(101); plot(x); title('x'); figure(102); plot(dx); title('dx'); figure(103); plot(ddx); title('ddx');
-    figure(104); plot(y); title('y');figure(105); plot(dy); title('dy'); figure(106); plot(ddy); title('ddy');
-    %figure(107); plot(1:length(k),k, [1,length(k)],CURVATURE_THRESH*ones(1,2)); title("|\kappa|");
-    figure(107); plot(1:crv_len,k, ...
+    figure(121); plot(x); title('x'); 
+    figure(122); plot(dx); title('dx'); 
+    figure(123); plot(ddx); title('ddx');
+    figure(124); plot(y); title('y');
+    figure(125); plot(dy); title('dy'); 
+    figure(126); plot(ddy); title('ddy');
+    figure(127); plot(1:crv_len,k, ...
                       [1,idmaxmid],kap_thresh1*ones(1,2), ...
                       [idmaxmid,crv_len],kap_thresh2*ones(1,2), ...
                       id1,min1,'ko', id2,min2,'ko', idmaxmid,maxmid,'ko'); 
     title("|\kappa|");
     ylim([0,max(k(wid+1:end-wid))*1.1])
-    %figure(108); plot(ddx.*ddy); title('prod of concavities');
+
+    tmp_idx = sub2ind([M,N],crv_xy(kap_zer,2),crv_xy(kap_zer,1));
+    tmp_im2 = zeros(M,N); tmp_im2(tmp_idx) = 1;
+    figure(130); imagesc(tmp_im2+tmp_im); axis image; colorbar;
 end
 
-tmp_idx = sub2ind([M,N],crv_xy(kap_zer,2),crv_xy(kap_zer,1));
-tmp_im2 = zeros(M,N); tmp_im2(tmp_idx) = 1;
-figure(702); imagesc(tmp_im2+tmp_im); axis image; colorbar;
-
-drawnow; 
-
-% TODO: pull out sections that include the minima
-
-%  pull out two longest sections with abs(kappa) > thresh
+%  pull out the two sections from above with small curvatur
 cc = bwconncomp(kap_zer);
-% IF ONLY ONE SEGMENT, CUT IT SOMEWHERE (HALFWAY?) AND FIT TWO LINES?
-% OR ASSUME ANGLE = 0 ? 
 if cc.NumObjects~=2
     %error("Fewer than two straight segments detected.");
     %warning("Number of straight segments detected is " + cc.NumObjects + "; should be TWO.");
@@ -301,6 +278,9 @@ if cc.NumObjects~=2
         end
     else
         warning("Only one straight line segment detected");
+        % TODO
+        % IF ONLY ONE SEGMENT, CUT IT SOMEWHERE (HALFWAY?) AND FIT TWO LINES?
+        % OR ASSUME ANGLE = 0 ?
     end
 else
     seg_1_idx = cc.PixelIdxList{1}; 
@@ -319,7 +299,7 @@ x_1 = 1:N; y_1 = beta_1(1) + beta_1(2)*x_1; % regression line
 x_1_data = X(:,2); 
 y_1_data = Y; 
 if PLOTS
-    figure(703); plot(X(:,2),Y,'b.', x_1,y_1,'r'); hold on
+    figure(128); plot(X(:,2),Y,'b.', x_1,y_1,'r'); hold on
     title("segment 1 fit line, y = " + round(beta_1(1)) + " + " + round(beta_1(2)) + " x");
     axis([0,N,0,M]);
 end
@@ -332,7 +312,7 @@ x_2 = 1:N; y_2 = beta_2(1) + beta_2(2)*x_2; % regression line
 x_2_data = X(:,2); 
 y_2_data = Y; 
 if PLOTS
-    figure(703); plot(X(:,2),Y,'b.', x_2,y_2,'r', crv_xy(:,1),crv_xy(:,2)); 
+    figure(128); plot(X(:,2),Y,'b.', x_2,y_2,'r', crv_xy(:,1),crv_xy(:,2)); 
     title({gca().Title.String, "segment 2 fit line, y = " + round(beta_2(1)) + " + " + round(beta_2(2)) + " x"});
     axis([0,N,0,M]); hold off;
 end
@@ -340,9 +320,10 @@ end
 %%
 % compute angle
 theta = atan(abs((beta_1(2)-beta_2(2))/(1+beta_1(2)*beta_2(2))))*180/pi;
+disp("File " + fn_name + " angle: " + theta + " degrees");
 
 % draw on photo
-figure(6); imagesc(im_orig_trim); axis image; axis off;
+figure(3); imagesc(im_orig_trim); axis image; axis off;
 hold on; plot(x_1,y_1, LineWidth=2); plot(x_2,y_2, LineWidth=2);
 plot(x_1_data, y_1_data, 'b', LineWidth=4);
 plot(x_2_data, y_2_data, 'r', LineWidth=4);
@@ -358,9 +339,8 @@ if SAVE
                           [[x_2(1),y_2(1)], [x_2(end),y_2(end)]], ...
                           'Color', LINE_COLOR, 'LineWidth', 8);
 
-    figure(2009); imshow(im_save);
+    figure(4); imshow(im_save);
 
-    [fn_pth,fn_name,~] = fileparts(fn);
     imwrite(im_save, fn_name + "_angle_" + ...
         floor(theta) + "_" + round((theta-floor(theta))*100) + ".png");
     % may want to add path (may change with gui to select images)
@@ -369,7 +349,7 @@ end
 %% HELPER FUNCTIONS BELOW THIS LINE
 
 %%
-function ret = smooth_th(arg, wid, fil)
+function ret = smooth_th(arg, wid, fil) %#ok<DEFNU>
     % use an odd number for wid
     arguments
         arg
